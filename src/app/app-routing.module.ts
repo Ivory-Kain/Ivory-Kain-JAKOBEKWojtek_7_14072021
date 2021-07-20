adc6c9ea17507d93c88d11560fc75c5f4e56b9fd
Wojtek <Teklin@hotmail.fr>
2021-07-20 16:57:06 +0200

    routes + architecture + login/signup pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ForumComponent } from './forum/forum.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  { path:'', component:HomeComponent },
+  { path:'login', component:LoginComponent },
+  { path:'signup', component:SignupComponent },
+  { path:'forum', component:ForumComponent },  
+  { path:'profile', component:ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ForumComponent } from './forum/forum.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForumComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
